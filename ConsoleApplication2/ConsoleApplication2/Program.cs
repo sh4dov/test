68855c2d30131d0fe8f11f3ccc9b7f92294dfb30
sh4dov <arojek.work@gmail.com>
2014-04-01 12:17:21 +0200

    SqLiteEF

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Reflection;
+﻿using System;
 
 namespace ConsoleApplication2
 {
@@ -7,7 +6,28 @@ namespace ConsoleApplication2
     {
         private static void Main(string[] args)
         {
-            Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsClass);
+            Method2((c1) =>
+                {
+                    Method1(ref c1);
+                    Console.WriteLine(c1.Name);
+                });
+        }
+
+        public static void Method2(Action<C1> action)
+        {
+            C1 c1 = new C1 { Name = "Method 2" };
+            action(ref c1);
+            Console.WriteLine(c1.Name);
+        }
+
+        public static void Method1(ref C1 name)
+        {
+            name = new C1 { Name = "Method 1" };
+        }
+
+        public class C1
+        {
+            public string Name { get; set; }
         }
     }
 }
---
+++ SqLiteEF/SqLiteEF/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SqLiteEF
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var db = new SqLiteDb();
+            int lastId = 0;
+
+            foreach (var entity in db.Entities)
+            {
+                Console.WriteLine(entity.Name);
+                lastId = entity.Id;
+            }
+
+            lastId++;
+            db.Entities.Add(new Entity { Name = string.Format("enity {0}", lastId) });
+            db.SaveChanges();
+        }
+    }
+
+    public class SqLiteDb : DbContext
+    {
+        public SqLiteDb()
+            : base("DatabaseContext")
+        {
+        }
+
+        public DbSet<Entity> Entities { get; set; }
+    }
+
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
